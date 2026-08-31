10d61789de2c41c75e4a1655ac0ad3e64678be25
priyanshu <pp8754352@gmail.com>
2026-08-31 13:56:46 +0530

    chnages like header2

--- colour.php
+++ colour.php
@@ -1,4 +1,4 @@
-<?php require_once __DIR__ . '/includes/header.php'; ?>
+<?php require_once __DIR__ . '/includes/header2.php'; ?>
 
 <div class="main-content app-content">
     <div class="container-fluid">
